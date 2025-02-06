d6425d295fe827f06cb5f3b0e78b1ee04c1b47cc
Einar Hagen <einarand@users.noreply.github.com>
2025-02-07 00:04:55 +0100

    Termostat class, power feature, settings, new icons, cleaning

--- drivers/Lay-Z/device.js
+++ drivers/Lay-Z/device.js
@@ -4,22 +4,31 @@ const Homey = require('homey');
 const axios = require('axios');
 
 class SpaDevice extends Homey.Device {
-    async onInit() {
-        this.log('SpaDevice has been initialized');
 
+    async onInit() {
+        this.log('SpaDevice is initializing...');
         this.headers = {
             'X-Gizwits-Application-Id': this.homey.settings.get('appId'),
             'Content-Type': 'application/json; charset=UTF-8',
         };
-
         this.baseUrl = this.homey.settings.get('baseUrl');
-
-        this.updateInterval = setInterval(() => {
-            this.updateOnlineStatus();
-            this.updateDeviceStatus();
-        }, 5 * 60 * 1000); // Update every 5 minutes
-        this.updateOnlineStatus();
-        this.updateDeviceStatus();
+        
+        //Remove legacy capabilities
+        await this.removeCapability('system_error').catch(this.error);
+        
+        //Setup capabilities
+        await this.enableCapability('onoff', this.getSetting('power_control_enabled'));
+        await this.enableCapability('pump_onoff', this.getSetting('filter_pump_control_enabled'));
+        await this.enableCapability('msg_onoff', this.getSetting('wave_control_enabled'));
+        await this.enableCapability('heat_temp_reach', true);
+        await this.enableCapability('pump_state', true);
+        await this.enableCapability('heat_state', true);
+        await this.enableCapability('temp_now', true);
+        await this.enableCapability('measure_power', true);
+        await this.enableCapability('measure_temperature', true);
+        await this.enableCapability('target_temperature', true);
+        await this.enableCapability('thermostat_mode', true);
+        await this.enableCapability('alarm_generic', true);
 
         // Register Capability Listeners
         this.log('Registering capability listeners...');
@@ -28,102 +37,127 @@ class SpaDevice extends Homey.Device {
         this.registerCapabilityListener('thermostat_mode', this.onCapabilityThermostatMode.bind(this));
         this.registerCapabilityListener('pump_onoff', this.onCapabilityPumpOnoff.bind(this));
         this.registerCapabilityListener('msg_onoff', this.onCapabilityMsgOnoff.bind(this));
-        this.registerCapabilityListener('system_error', this.onCapabilitySystemError.bind(this));
         this.log('Capability listeners registered');
+
+        // Start the update interval
+        this.updateInterval = setInterval(async () => {
+            await this.updateOnlineStatus();
+            await this.updateDeviceStatus();
+        }, 1 * 60 * 1000); // Update every 1 minutes
+        await this.updateOnlineStatus();
+        await this.updateDeviceStatus();
+        
+        this.log('SpaDevice has been initialized');
+    }
+    
+    async enableCapability(capability, enabled) {
+        if (enabled && !this.hasCapability(capability)) {
+            await this.addCapability(capability).catch(this.error);
+        } else if (!enabled && this.hasCapability(capability)) {
+            await this.removeCapability(capability).catch(this.error);
+        }
     }
 
     async updateDeviceStatus() {
         this.log('updateDeviceStatus started');
 
         try {
-            const token = this.homey.settings.get('token');
-            const data = await this.makeAxiosCall('get', `${this.baseUrl}/app/devdata/${this.getData().did}/latest`, null, token);
-            const { attr } = data;
+            const data = await this.fetchDeviceData();
+            const {attr} = data;
 
             if (!attr) {
-                throw new Error("attr field missing in response data");
+                throw new Error('attr field missing in response data');
             }
+            
+            const powerOn = attr.power === 1;
+            const thermostatOn = attr.heat_power === 1;
+            const heatTempReached = attr.heat_temp_reach === 1;
+            const heaterActive = thermostatOn & !heatTempReached;
+            const filterPumpOn = attr.filter_power === 1;
+            const wavePowerOn = attr.wave_power === 1;
+            const heaterWatts = this.getSetting('heater_power');
+            const filterPumpWatts = this.getSetting('filter_pump_power');
+            const powerConsumption = heaterActive * heaterWatts + filterPumpOn * filterPumpWatts;
 
-            const temp_now = attr.temp_now;
-            const temp_set = attr.temp_set;
+            const {
+                temp_now: currentTemperature,
+                temp_set: temperatureSetpoint,
+            } = attr;
+            
             const units = attr.temp_set_unit === '华氏' ? '°F' : '°C';
-
-            // Update temp_now directly
-            this.safeSetCapabilityValue('temp_now', temp_now);
-            this.safeSetCapabilityValue('target_temperature', temp_set);
-
             // Dynamically set units and ranges for capabilities
             const options = {
                 units: {
-                    en: units,
-                    no: units
-                },
-                min: units === '°F' ? 68 : 20,
-                max: units === '°F' ? 104 : 40,
-                step: 1
+                    en: units, no: units,
+                }, 
+                min: units === '°F' ? 68 : 20, max: units === '°F' ? 104 : 40, step: 1,
+                decimals: 0,
             };
+            await this.setCapabilityOptions('target_temperature', options);
+            await this.safeSetCapabilityValue('target_temperature', thermostatOn ? temperatureSetpoint : null);
+            await this.safeSetCapabilityValue('measure_temperature', currentTemperature);
+            await this.safeSetCapabilityValue('thermostat_mode', thermostatOn ? 'heat' : 'off');
+            await this.safeSetCapabilityValue('temp_now', currentTemperature);
             
-            this.setCapabilityOptions('target_temperature', options);
-            
+            await this.safeSetCapabilityValue('onoff', powerOn);
+            await this.safeSetCapabilityValue('pump_onoff', filterPumpOn);
+            await this.safeSetCapabilityValue('msg_onoff', wavePowerOn);
+            await this.safeSetCapabilityValue('heat_temp_reach', heatTempReached);
+            await this.safeSetCapabilityValue('pump_state', filterPumpOn);
+            await this.safeSetCapabilityValue('heat_state', thermostatOn);
+            await this.safeSetCapabilityValue('measure_power', powerConsumption);
 
-
-            this.safeSetCapabilityValue('onoff', !!attr.power);
-            this.safeSetCapabilityValue('thermostat_mode', attr.heat_power ? 'heat' : 'cool');
-            this.safeSetCapabilityValue('pump_onoff', !!attr.filter_power);
-            this.safeSetCapabilityValue('msg_onoff', !!attr.wave_power);
-            this.safeSetCapabilityValue('heat_temp_reach', !!attr.heat_temp_reach);
-            this.safeSetCapabilityValue('pump_state', !!attr.filter_power);
-            this.safeSetCapabilityValue('heat_state', !!attr.heat_power);
-
-            this.log('Setting initial system_error value');
-            this.safeSetCapabilityValue('system_error', false);
-
-            this.updateSystemErrors(attr);
-
+            await this.updateSystemErrors(attr);
         } catch (error) {
             if (error.response) {
                 this.error(`Server responded with status code: ${error.response.status}`, error.message);
             } else if (error.request) {
-                this.error("No response received from the server", error.message);
+                this.error('No response received from the server', error.message);
             } else {
                 this.error('Failed to update device status', error.message);
             }
         }
     }
 
-    updateSystemErrors(attr) {
-        this.log('updateSystemErrors started');
-        let errors = [];
+    async fetchDeviceData() {
+        const token = this.homey.settings.get('token');
+        if (!token) {
+            throw new Error('No token found in settings');
+        }
+        return await this.makeAxiosCall('get', `${this.baseUrl}/app/devdata/${this.getData().did}/latest`, null, token);
+    }
+
+    async updateSystemErrors(attr) {
+        const errors = [];
         for (let i = 1; i <= 9; i++) {
             if (attr[`system_err${i}`] === 1) {
                 errors.push(`E0${i}`);
             }
         }
         const hasErrors = errors.length > 0;
-        this.safeSetCapabilityValue('system_error', hasErrors);
-
+        await this.safeSetCapabilityValue('alarm_generic', hasErrors);
         if (hasErrors) {
-            this.setWarning('System error(s): ' + errors.join(', '));
+            await this.setWarning(`System error(s): ${errors.join(', ')}`);
         } else {
-            this.setWarning('');
+            await this.unsetWarning();
         }
     }
 
     async updateOnlineStatus() {
         this.log('updateOnlineStatus started');
         const token = this.homey.settings.get('token');
-
         try {
             const response = await axios.get(`${this.baseUrl}/app/bindings`, {
-                headers: { ...this.headers, 'X-Gizwits-User-token': token },
+                // eslint-disable-next-line node/no-unsupported-features/es-syntax
+                headers: {...this.headers, 'X-Gizwits-User-token': token},
             });
 
-            const device = response.data.devices.find(device => device.did === this.getData().did);
+            const device = response.data.devices.find((device) => device.did === this.getData().did);
             if (device) {
                 if (device.is_online) {
-                    this.setAvailable();
+                    await this.setAvailable();
                 } else {
-                    this.setUnavailable();
+                    await this.setUnavailable();
                 }
             } else {
                 this.log(`Device with did ${this.getData().did} not found`);
@@ -133,31 +167,33 @@ class SpaDevice extends Homey.Device {
         }
     }
 
-    async makeAxiosCall(method, url, data, token) {
+    async makeAxiosCall(method, url, data = null, token = null) {
         try {
-            const response = await axios({ method, url, data, headers: { ...this.headers, 'X-Gizwits-User-token': token } });
+            const headers = { ...this.headers };
+            if (token) {
+                headers['X-Gizwits-User-token'] = token;
+            }
+            const response = await axios({ method, url, data, headers });
             return response.data;
         } catch (error) {
-            this.log(`Failed to make ${method} request`, error);
+            const status = error.response?.status || 'unknown';
+            this.log(`Failed to make ${method} request to ${url} (Status: ${status})`, error.message);
+            return null; // Ensures failure handling in calling code
         }
     }
 
-    safeSetCapabilityValue(capability, value) {
-        if (value !== undefined && value !== null) {
-            try {
-                this.setCapabilityValue(capability, value);
-            } catch (error) {
-                this.error(`Failed to update ${capability}`, error);
-            }
-        } else {
-            this.error(`Value for ${capability} is missing or null.`);
+    async safeSetCapabilityValue(capability, value) {
+        if (!this.hasCapability(capability)) {
+            return;
         }
+        await this.setCapabilityValue(capability, value)
+            .catch(e => this.error(`Failed to update ${capability}`, e));
     }
 
     async onCapabilityTargetTemperature(value) {
         try {
             const token = this.homey.settings.get('token');
-            await this.makeAxiosCall('post', `${this.baseUrl}/app/control/${this.getData().did}`, { attrs: { temp_set: value } }, token);
+            await this.makeAxiosCall('post', `${this.baseUrl}/app/control/${this.getData().did}`, {attrs: {temp_set: value}}, token);
             await this.updateDeviceStatus();
         } catch (error) {
             this.error('Failed to set target temperature', error);
@@ -170,8 +206,7 @@ class SpaDevice extends Homey.Device {
             if (!token) {
                 throw new Error('No token found in settings');
             }
-
-            await this.makeAxiosCall('post', `${this.baseUrl}/app/control/${this.getData().did}`, { attrs: { power: value ? 1 : 0 } }, token);
+            await this.makeAxiosCall('post', `${this.baseUrl}/app/control/${this.getData().did}`, {attrs: {power: value ? 1 : 0}}, token);
             await this.updateDeviceStatus();
         } catch (error) {
             this.error('Failed to change on/off status', error);
@@ -179,47 +214,26 @@ class SpaDevice extends Homey.Device {
     }
 
     async onCapabilityThermostatMode(value) {
+        switch (value) {
+            case 'heat':
+                await this.setHeatPower(1);
+                break;
+            case 'off':
+                await this.setHeatPower(0);
+                break;
+            default:
+                this.log('Unsupported value');
+                break;
+        }
+    }
+
+    async setHeatPower(value) {
         try {
             const token = this.homey.settings.get('token');
             if (!token) {
                 throw new Error('No token found in settings');
             }
-
-            const data = await this.makeAxiosCall('get', `${this.baseUrl}/app/devdata/${this.getData().did}/latest`);
-            if (!data || !data.attr || data.attr.heat_power === undefined) {
-                throw new Error('Invalid data received from server');
-            }
-
-            switch (value) {
-                case 'auto':
-                case 'heat':
-                    if (!data.attr.heat_power) {
-                        await this.setHeatPowerStatus(1, token);
-                    }
-                    break;
-                case 'cool':
-                    if (data.attr.heat_power) {
-                        await this.setHeatPowerStatus(0, token);
-                    }
-                    break;
-                case 'off':
-                    await this.onCapabilityPumpOnoff(false);
-                    break;
-            }
-
-            await this.updateDeviceStatus();
-        } catch (error) {
-            this.error('Failed to change thermostat mode', error);
-        }
-    }
-
-    async setHeatPowerStatus(value, token) {
-        try {
-            if (!token) {
-                throw new Error('No token found in settings');
-            }
-
-            await this.makeAxiosCall('post', `${this.baseUrl}/app/control/${this.getData().did}`, { attrs: { heat_power: value } }, token);
+            await this.makeAxiosCall('post', `${this.baseUrl}/app/control/${this.getData().did}`, {attrs: {heat_power: value}}, token);
             await this.updateDeviceStatus();
         } catch (error) {
             this.error('Failed to set heating power status', error);
@@ -232,8 +246,7 @@ class SpaDevice extends Homey.Device {
             if (!token) {
                 throw new Error('No token found in settings');
             }
-
-            await this.makeAxiosCall('post', `${this.baseUrl}/app/control/${this.getData().did}`, { attrs: { filter_power: value ? 1 : 0 } }, token);
+            await this.makeAxiosCall('post', `${this.baseUrl}/app/control/${this.getData().did}`, {attrs: {filter_power: value ? 1 : 0}}, token);
             await this.updateDeviceStatus();
         } catch (error) {
             this.error('Failed to toggle filter pump', error);
@@ -246,17 +259,11 @@ class SpaDevice extends Homey.Device {
             if (!token) {
                 throw new Error('No token found in settings');
             }
-
-            await this.makeAxiosCall('post', `${this.baseUrl}/app/control/${this.getData().did}`, { attrs: { wave_power: value ? 1 : 0 } }, token);
+            await this.makeAxiosCall('post', `${this.baseUrl}/app/control/${this.getData().did}`, {attrs: {wave_power: value ? 1 : 0}}, token);
             await this.updateDeviceStatus();
         } catch (error) {
             this.error('Failed to toggle massage function', error);
         }
-    }
-
-    async onCapabilitySystemError(value) {
-        this.log(`System error status changed to ${value}`);
-        // Additional logic to handle system error changes if needed
     }
 
     async onDeleted() {
@@ -264,13 +271,20 @@ class SpaDevice extends Homey.Device {
         this.log('SpaDevice has been deleted');
     }
 
-    async onPoll() {
-        this.log('Polling for new data');
-        this.log(`Polling device ${this.getData().did}`);
-
-        this.updateOnlineStatus();
-        this.updateDeviceStatus();
+    async onSettings({ oldSettings, newSettings, changedKeys }) {
+        console.log('Settings changed:', {oldSettings, newSettings, changedKeys});
+        if (changedKeys.includes('power_control_enabled')) {
+            await this.enableCapability('onoff', newSettings.power_control_enabled);
+        }
+        if (changedKeys.includes('filter_pump_control_enabled')) {
+            await this.enableCapability('pump_onoff', newSettings.filter_pump_control_enabled);
+        }
+        if (changedKeys.includes('wave_control_enabled')) {
+            await this.enableCapability('msg_onoff', newSettings.wave_control_enabled);
+        }
+        await this.updateDeviceStatus();
     }
+
 }
 
 module.exports = SpaDevice;
